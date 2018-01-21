7190e912b3fb6f25f1ec8fd8918a9daf34686dfc
hansffu <hansff@gmail.com>
2018-01-21 20:47:16 +0100

    Make Deg2UTM a bit prettier

--- app/src/main/java/hansffu/ontime/conversion/Deg2UTM.kt
+++ app/src/main/java/hansffu/ontime/conversion/Deg2UTM.kt
@@ -1,38 +1,36 @@
 package hansffu.ontime.conversion
 
 class Deg2UTM(Lat: Double, Lon: Double) {
-    var easting: Double = 0.toDouble()
+    var easting: Double
         private set
-    var northing: Double = 0.toDouble()
+    var northing: Double
         private set
     val zone: Int = Math.floor(Lon / 6 + 31).toInt()
-    var letter: Char = ' '
-        private set
+
+    val letter: Char = when {
+        Lat < -72 -> 'C'
+        Lat < -64 -> 'D'
+        Lat < -56 -> 'E'
+        Lat < -48 -> 'F'
+        Lat < -40 -> 'G'
+        Lat < -32 -> 'H'
+        Lat < -24 -> 'J'
+        Lat < -16 -> 'K'
+        Lat < -8 -> 'L'
+        Lat < 0 -> 'M'
+        Lat < 8 -> 'N'
+        Lat < 16 -> 'P'
+        Lat < 24 -> 'Q'
+        Lat < 32 -> 'R'
+        Lat < 40 -> 'S'
+        Lat < 48 -> 'T'
+        Lat < 56 -> 'U'
+        Lat < 64 -> 'V'
+        Lat < 72 -> 'W'
+        else -> 'X'
+    }
 
     init {
-        when {
-            Lat < -72 -> letter = 'C'
-            Lat < -64 -> letter = 'D'
-            Lat < -56 -> letter = 'E'
-            Lat < -48 -> letter = 'F'
-            Lat < -40 -> letter = 'G'
-            Lat < -32 -> letter = 'H'
-            Lat < -24 -> letter = 'J'
-            Lat < -16 -> letter = 'K'
-            Lat < -8 -> letter = 'L'
-            Lat < 0 -> letter = 'M'
-            Lat < 8 -> letter = 'N'
-            Lat < 16 -> letter = 'P'
-            Lat < 24 -> letter = 'Q'
-            Lat < 32 -> letter = 'R'
-            Lat < 40 -> letter = 'S'
-            Lat < 48 -> letter = 'T'
-            Lat < 56 -> letter = 'U'
-            Lat < 64 -> letter = 'V'
-            Lat < 72 -> letter = 'W'
-            else -> letter = 'X'
-        }
-
         easting = 0.5 * Math.log((1 + Math.cos(Lat * Math.PI / 180) * Math.sin(Lon * Math.PI / 180 - (6 * zone - 183) * Math.PI / 180)) / (1 - Math.cos(Lat * Math.PI / 180) * Math.sin(Lon * Math.PI / 180 - (6 * zone - 183) * Math.PI / 180))) * 0.9996 * 6399593.62 / Math.pow(1 + Math.pow(0.0820944379, 2.0) * Math.pow(Math.cos(Lat * Math.PI / 180), 2.0), 0.5) * (1 + Math.pow(0.0820944379, 2.0) / 2 * Math.pow(0.5 * Math.log((1 + Math.cos(Lat * Math.PI / 180) * Math.sin(Lon * Math.PI / 180 - (6 * zone - 183) * Math.PI / 180)) / (1 - Math.cos(Lat * Math.PI / 180) * Math.sin(Lon * Math.PI / 180 - (6 * zone - 183) * Math.PI / 180))), 2.0) * Math.pow(Math.cos(Lat * Math.PI / 180), 2.0) / 3) + 500000
         easting = Math.round(easting * 100) * 0.01
 
