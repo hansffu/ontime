2c4a77f269220becf1a1f51f65a14f53cd820f99
Hans Fredrik Furholt <hansff@gmail.com>
2024-07-26 00:11:11 +0200

    use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,13 @@
-val apolloVersion = "3.1.0"
-val room_version = "2.6.1"
-val accompanistVersion = "0.35.1-alpha"
-
 plugins {
-    id("com.android.application")
-    kotlin("android")
-    id("com.google.devtools.ksp") version "2.0.0-1.0.23"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
-    id("com.apollographql.apollo3") version "3.8.5"
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.apollo)
 }
 
 android {
     compileSdk = 34
-    buildToolsVersion = "30.0.3"
     defaultConfig {
         applicationId = "hansffu.ontime"
         minSdk = 26
@@ -33,7 +28,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin{
+    kotlin {
         jvmToolchain(17)
     }
     kotlinOptions {
@@ -42,14 +37,14 @@ android {
 
     namespace = "hansffu.ontime"
     composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+        kotlinCompilerExtensionVersion = libs.versions.compose.get()
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-   compileOptions {
+    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -57,53 +52,53 @@ android {
 
 dependencies {
     implementation(fileTree("include" to "*.jar", "dir" to "libs"))
-    implementation("com.google.android.support:wearable:2.9.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    compileOnly("com.google.android.wearable:wearable:2.9.0")
-    implementation("com.google.android.gms:play-services-wearable:18.2.0")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("androidx.wear:wear:1.3.0")
-    implementation("androidx.wear:wear-input:1.1.0")
-    implementation("androidx.activity:activity-ktx:1.9.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("com.apollographql.apollo3:apollo-runtime:3.8.5")
-    compileOnly("org.jetbrains:annotations:23.0.0")
+    implementation(libs.support.wearable)
+    implementation(libs.material)
+    implementation(libs.lifecycle.runtime.ktx)
+    compileOnly(libs.wearable)
+    implementation(libs.play.services.wearable)
+    implementation(libs.play.services.location)
+    implementation(libs.androidx.wear)
+    implementation(libs.wear.input)
+    implementation(libs.activity.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.apollo.runtime)
+    compileOnly(libs.jetbrains.annotations)
 
-    implementation("androidx.room:room-runtime:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
-    implementation("org.jetbrains:annotations:23.0.0")
+    implementation(libs.jetbrains.annotations)
 
     //compose
-    implementation("androidx.activity:activity-compose:1.9.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui.tooling.preview)
 
-    implementation("androidx.wear.compose:compose-material:1.3.1")
-    implementation("androidx.wear.compose:compose-foundation:1.3.1")
-    implementation("androidx.wear.compose:compose-navigation:1.3.1")
-    implementation("androidx.compose.material:material:1.6.8")
-    implementation("androidx.compose.material:material-icons-extended:1.6.8")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
+    implementation(libs.androidx.wear.compose.material)
+    implementation(libs.androidx.wear.compose.foundation)
+    implementation(libs.androidx.wear.compose.navigation)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.runtime.livedata)
 
-    implementation("com.google.accompanist:accompanist-swiperefresh:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-placeholder-material:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+    implementation(libs.accompanist.swiperefresh)
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.placeholder.material)
+    implementation(libs.accompanist.permissions)
 
-    implementation("com.google.android.horologist:horologist-compose-layout:0.6.16")
-    implementation("com.google.android.horologist:horologist-compose-material:0.6.16")
+    implementation(libs.horologist.compose.layout)
+    implementation(libs.horologist.compose.material)
 
-    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
 }
 
 apollo {
-    service("entur"){
+    service("entur") {
         packageName.set("hansffu.ontime.graphql")
         customScalarsMapping.set(mapOf("DateTime" to "java.time.OffsetDateTime"))
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-  val compose_version by extra("1.6.8")
-  val kotlin_version = "2.0.0"
-
-  extra.apply {
-    set("kotlin_version", "2.0.0")
-  }
-  repositories {
-    google()
-    mavenCentral()
-  }
-  dependencies {
-    classpath("com.android.tools.build:gradle:8.3.2")
-    classpath(kotlin("gradle-plugin", version = kotlin_version))
-
-    // NOTE: Do not place your application dependencies here; they belong
-    // in the individual module build.gradle files
-  }
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.apollo) apply false
 }
 
 allprojects {
-  repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
-  }
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 
